1f6c3dedeb38e05af6700ff3987abd15062c4288
Sadakat Hossain <md.hossain.sadakat@gmail.com>
2023-08-08 22:22:47 +0600

    connect with MongoDB

--- index.js
+++ index.js
@@ -6,7 +6,35 @@ const port = process.env.PORT || 5000;
 
 // Use Middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+// Connect With MongoDB(NoSQL)
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwt8lkk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 
@@ -14,10 +42,6 @@ app.get('/', (req, res) => {
     res.send('Food is cooking')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Food is cooking on port ${port}`)
 })
-
-
-// SadakatHossain
-// G7A1QQnXWMyxdI3X
