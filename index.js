caee10a62b9e77beca4994595f02f44dd23489b9
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-08-19 19:51:44 +0600

    Setup  appget for cart collction api

--- index.js
+++ index.js
@@ -43,11 +43,21 @@ async function run() {
       res.send(result);
     })
 
-    // cart collection
-    app.post('/carts', async(req,res) => {
-      const item =req.body;
+    // cart collection apis:
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/carts', async (req, res) => {
+      const item = req.body;
       console.log(item);
-      const result  = await cartCollection.insertOne(item);
+      const result = await cartCollection.insertOne(item);
       res.send(result);
     })
 
