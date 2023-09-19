86f4620d58ce279cb81c82e6c54d72f7b3064a47
Sadakat Hossain <sadakat.hossain11@gmail.com>
2023-09-19 21:28:56 +0700

    Last commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Connect With MongoDB(NoSQL)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwt8lkk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,15 +29,25 @@ async function run() {
 
 
     // send result
+    const usersCollection = client.db("FoodVozoniDB").collection("users");
     const menuCollection = client.db("FoodVozoniDB").collection("menu");
     const reviewsCollection = client.db("FoodVozoniDB").collection("reviews");
     const cartCollection = client.db("FoodVozoniDB").collection("carts");
 
+    // Users releted APIs
+    app.post('/users', async(req, res) => {
+      const user = req.body;
+      const result = usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    // Menu releted APIs
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result)
     })
 
+    // Review releted APIs
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
@@ -58,6 +68,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
